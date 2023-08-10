623ea8d2421f018a4da5e68e931a333cb392bc4b
Zaheer <112798052+Zaheer-10@users.noreply.github.com>
2023-08-10 20:39:18 +0530

    resources

--- PaperMate_ui/GUI/urls.py
+++ PaperMate_ui/GUI/urls.py
@@ -3,11 +3,14 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('qa/', views.qa_page, name='qa'),
     path('recommendations/' , views.recommendations, name='recommendations'),
     path('search/', views.search_papers, name='search_papers'),
+    # path('<str:category>/', views.index, name='index_category'),
     # path('recommendation/', views.recommendation, name='recommendation'),
 ]
 # Serve static files during development
