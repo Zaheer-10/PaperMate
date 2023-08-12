c52e8b7274d79ddbaf96c1e7a244508e155632f5
Zaheer <112798052+Zaheer-10@users.noreply.github.com>
2023-08-12 20:37:02 +0530

    Model registered

--- PaperMate_ui/GUI/admin.py
+++ PaperMate_ui/GUI/admin.py
@@ -1,20 +1,26 @@
-from django.db import transaction
-from django.contrib import admin
-from .models import Paper
 import math
+from .models import Paper
 from .models import RecentPaper
+from django.contrib import admin
+from django.db import transaction
 
 
 @admin.register(Paper)
 class PaperAdmin(admin.ModelAdmin):
+    """
+    Admin interface configuration for the 'Paper' model.
+    """
+    # Display these fields in the admin list view.
     list_display = ('title', 'abstract', 'terms', 'url', 'ids')
+    
+    # Add a custom action for removing duplicates.
     actions = ['remove_duplicates']
 
     @transaction.atomic
     def remove_duplicates(self, request, queryset):
         seen_titles = set()
         duplicates = []
-        batch_size = 1000  # Adjust the batch size as needed
+        batch_size = 1000  
         total_duplicates = queryset.count()
         batches = math.ceil(total_duplicates / batch_size)
 
@@ -33,4 +39,7 @@ class PaperAdmin(admin.ModelAdmin):
 
     remove_duplicates.short_description = "Remove selected duplicates"
 
+
+
+# Register the 'RecentPaper' model with the default admin interface.
 admin.site.register(RecentPaper)
