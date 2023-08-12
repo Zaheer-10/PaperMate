4c88eb4bd5f0eb916bec0f3f76608b298466c412
Zaheer <112798052+Zaheer-10@users.noreply.github.com>
2023-08-12 20:36:45 +0530

    Final update

--- PaperMate_ui/GUI/urls.py
+++ PaperMate_ui/GUI/urls.py
@@ -1,19 +1,17 @@
-from django.urls import path
 from . import views
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
+# Configure URL patterns for the web application
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('qa/', views.qa_page, name='qa'),
-    path('recommendations/' , views.recommendations, name='recommendations'),
-    path('about/', views.about, name='about'),
-    path('search/', views.search_papers, name='search_papers'),
-    # path('<str:category>/', views.index, name='index_category'),
-    # path('recommendation/', views.recommendation, name='recommendation'),
-    ]
+    path('', views.index, name='index'),  # Home page
+    path('search/', views.search_papers, name='search_papers'),  # Search papers page
+    path('recommendations/', views.recommendations, name='recommendations'),  # Recommendations page
+    path('about/', views.about, name='about'),  # About page
+    path('qa/', views.qa_page, name='qa'),  # Q&A page
+]
+
 # Serve static files during development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
